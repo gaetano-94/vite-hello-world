be94ebfdfdc80940aa466bfa8009745a2bd5f081
gaetano-94 <gaetano.panico1994@gmail.com>
2023-12-06 19:19:55 +0100

    feat: title con vite e vue

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  name: 'AppTitleProva',
+  data() {
+    return {
+      title: 'Provo un titolo con Vite e Vue',
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<style scoped></style>
